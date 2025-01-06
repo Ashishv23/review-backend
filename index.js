e4e40172ed42e5bd663e3b1deb068a5bb5096e32
Ashish Vaghela <ashish.vaghela2311@gmail.com>
2025-01-06 00:05:56 -0500

    update index.js.

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
-import ReviewsDAO from "./dao/reviewsDAO.js"
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 dotenv.config();
 
 const mongoClient = mongodb.MongoClient;
 const mongo_username = process.env.DB_USERNAME;
 const mongo_passwrod = process.env.DB_PASSWORD;
-const uri = `mongodb+srv://${mongo_username}:${mongo_passwrod}@cluster0.0qliqrh.mongodb.net/`
+const uri = `mongodb+srv://${mongo_username}:${mongo_passwrod}@cluster0.0qliqrh.mongodb.net/`;
 
 const port = 8000;
 
-mongoClient.connect(
-    uri,
-    {
-        maxPoolSize: 50,
-        wtimeoutMS: 2500,
-        // useNewUrlParser: true
-    }
-).catch(err => {
+mongoClient
+  .connect(uri, {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+    // useNewUrlParser: true
+  })
+  .catch((err) => {
     console.error(err.stack);
     process.exit(1);
-}).then(async client => {
+  })
+  .then(async (client) => {
     await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
-        console.log(`listening on port ${port}`);
+      console.log(`listening on port https://localhost:${port}`);
     });
-});
+  });
